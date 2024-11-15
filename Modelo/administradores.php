2289ecc95e3cf3cafefe91c8ea37f2ed611a3751
MicsC18 <132679351+MicsC18@users.noreply.github.com>
2024-11-15 09:35:48 -0300

    Update administradores.php

--- Modelo/administradores.php
+++ Modelo/administradores.php
@@ -37,5 +37,64 @@
                 return false; 
             }
         }
+
+        public function verificarAdministrador($id_administrador, $nombre) {
+            
+            $sql = "SELECT 1 FROM administradores WHERE id_administrador = :id_administrador AND nombre = :nombre LIMIT 1";
+            
+            // Preparamos la consulta
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->bindParam(':id_administrador', $id_administrador, PDO::PARAM_INT);
+            $stmt->bindParam(':nombre', $nombre, PDO::PARAM_STR);
+            
+            try {
+                $stmt->execute();
+                if ($stmt->rowCount() > 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (PDOException $e) {
+                error_log('Error al verificar administrador: ' . $e->getMessage());
+                return false;
+            }
+        }
+
+        public function ObtenerClave($id_administrador) {
+        
+            $sql = "SELECT clave FROM administradores WHERE id_administrador = :id_administrador";
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->bindParam(':id_administrador', $id_administrador, PDO::PARAM_INT);
+            
+            if ($stmt->execute()) {
+                $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($resultado) {
+                    // Retornar solo la clave
+                    return $resultado['clave'];
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        public function eliminar($id_administrador) {
+            $sql = "DELETE FROM administradores WHERE id_administrador = :id_administrador";
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->bindParam(':id_administrador', $id_administrador);
+        
+            try {
+                $stmt->execute();
+                return $stmt->rowCount() > 0;
+            } catch (PDOException $e) {
+                error_log("Error al eliminar al administrador: " . $e->getMessage());
+                return false; 
+            }
+            
+        }
+    
     }
+
+        
 ?>
